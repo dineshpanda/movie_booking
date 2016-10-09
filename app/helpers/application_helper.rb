ca74314c8f322af4a611bfad979fca2b7b749086
dineshpanda92 <dinesh.panda@mindfiresolutions.com>
2016-10-09 17:46:29 +0530

    Added seed data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,5 @@ module ApplicationHelper
       "info"
     end
   end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Create movies
+first_movie = Movie.create title: 'MS Dhoni', release_date: '30/09/2016', ticket_fee: '150'.to_d
+second_movie = Movie.create title: 'Pink', release_date: '16/09/2016', ticket_fee: '180'.to_d
+
+#Create theaters
+inox = Theater.create name: 'INOX'
+pvr = Theater.create name: 'PVR'
+cinepolis = Theater.create name: 'Cinepolis'
+
+#Create Coupons
+Coupon.create code: 'mb40', amount: '40'.to_d
+Coupon.create code: 'mb60', amount: '60'.to_d
+
+#Assigning/Screening movies in theaters
+inox.movies << first_movie
+inox.movies << second_movie
+
+pvr.movies << first_movie
+pvr.movies << second_movie
+
+cinepolis.movies << first_movie
